91bccbc14a06f7a8d1bd0ce0403179862ddf1583
Mindaugas <strumke@yahoo.com>
2020-01-21 15:42:23 +0100

    Updates

---
+++ precache-manifest.d24a2f1d0cf36a0d92117a3a4ef12241.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0779ab33e009c2fda66713353e87ecc4",
+    "url": "/unsplash-fetcher/index.html"
+  },
+  {
+    "revision": "30e9a201d4e29dcc7382",
+    "url": "/unsplash-fetcher/static/css/main.6952ddf5.chunk.css"
+  },
+  {
+    "revision": "30de06cd3646fdd9e3ba",
+    "url": "/unsplash-fetcher/static/js/2.5fe800a8.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/unsplash-fetcher/static/js/2.5fe800a8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "30e9a201d4e29dcc7382",
+    "url": "/unsplash-fetcher/static/js/main.7278bb69.chunk.js"
+  },
+  {
+    "revision": "a8b51530685d90fe2373",
+    "url": "/unsplash-fetcher/static/js/runtime-main.20ee9dda.js"
+  }
+]);
